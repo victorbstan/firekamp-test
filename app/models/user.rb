2e8d7767fdcc4fdc24ff46d4c59b405c377c3bc9
Victor Stan <victor.stan@gmail.com>
2024-04-19 12:55:55 -0400

    user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable
 
+  validates :email, presence: true, length: { maximum: 500 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { in: 6..20 }
+
   def authenticate(password)
     # using devise method
     valid_password? password
