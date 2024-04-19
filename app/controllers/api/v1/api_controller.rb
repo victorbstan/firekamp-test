10bdf4f7e7cd16ada2f88b5a511391ab1d288e9e
Victor Stan <victor.stan@gmail.com>
2024-04-19 17:16:33 -0400

    controller updates

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -4,64 +4,27 @@ module Api
   module V1
     class ApiController < ActionController::API
       rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+      # rescue_from Exception, with: :internal_server_error
 
-      def encode_token(payload)
-        payload['exp'] = expiration_time
-        JWT.encode(payload, encode_string)
-      end
-
-      def auth_header
-        # { Authorization 'Bearer <token>' }
-        request.headers['Authorization']
-      end
-
-      def decoded_token # rubocop:disable Metrics/MethodLength
-        return unless auth_header
-
-        token = auth_header.split(' ')[1]
-
-        # header: { 'Authorization': 'Bearer <token>' }
+      def authorize_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
         begin
-          JWT.decode(token, encode_string, true, algorithm: 'HS256')
-        rescue JWT::ExpiredSignature => e
-          # Handle expired token, e.g. logout user or deny access
-          logger.debug("JWT EXPIRED #{e}")
-          nil
+          decoded = JsonWebToken.decode(header)
+          @current_user = User.find(decoded[:user_id])
+        rescue ActiveRecord::RecordNotFound => e
+          render json: { error: e.message }, status: :unauthorized
         rescue JWT::DecodeError => e
-          # Other errors
-          logger.debug("JWT ERROR #{e}")
-          nil
+          render json: { error: e.message }, status: :unauthorized
         end
       end
 
-      def current_user
-        return unless decoded_token
-
-        user_id = decoded_token[0]['user_id']
-        @user = User.find_by(id: user_id)
-        @user
-      end
-
-      def logged_in?
-        !!current_user
-      end
-
-      def authorized
-        return if logged_in?
-
-        render json: { message: 'Please log in' }, status: :unauthorized
-      end
-
-      def encode_string
-        ENV['API_SECRET_TOKEN']
-      end
-
-      def expiration_time
-        Time.now.to_i + 10.minutes
-      end
-
       def record_not_found
-        render json: { message: 'Record not found' }, status: :not_found
+        render json: { error: 'Record not found' }, status: :not_found
+      end
+
+      def internal_server_error
+        render json: { error: 'Internal server error' }, status: 500
       end
     end
   end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,40 +3,32 @@
 module Api
   module V1
     class UsersController < Api::V1::ApiController
-      before_action :authorized, only: [:auto_login]
+      before_action :authorize_request, except: %i[register login]
 
       def register
-        @user = User.new(user_params)
-        if @user.save
-          token = encode_token({ user_id: @user.id })
-          render json: { user: @user, token: }
+        user = User.new(email: user_params[:email], password: user_params[:password])
+        if user.save
+          token = JsonWebToken.encode(user_id: user.id)
+          render json: user, token:, serializer: UserSerializer, status: :created
         else
-          render json: { error: 'Invalid email or password' }
+          render json: { error: user.errors.full_messages }, status: :bad_request
         end
       end
 
       def login
-        @user = User.find_by(email: user_params[:email])
-        # if @user&.authenticate(user_params[:password])
-        if UserService::UserAuthenticator.call(@user, user_params[:password])
-          # add expiration time to token
-          @user.touch
-          @user.save!
-          token = encode_token({ user_id: @user.id })
-          render json: { user: @user, token: }
+        user = User.find_by(email: user_params[:email])
+        if UserService::UserAuthenticator.call(user, user_params[:password])
+          token = JsonWebToken.encode(user_id: user.id)
+          render json: user, token:, serializer: UserSerializer, status: :ok
         else
-          render json: { error: 'Invalid email or password' }
+          render json: { error: 'Invalid email or password' }, status: :unauthorized
         end
-      end
-
-      def auto_login
-        render json: @user
       end
 
       private
 
       def user_params
-        params.permit(:email, :password)
+        params.permit(:email, :password, user: {})
       end
     end
   end
