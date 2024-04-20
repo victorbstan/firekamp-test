bbb9dcdfc696f1e5712c74898b02bac85055f940
Victor Stan <victor.stan@gmail.com>
2024-04-19 22:11:06 -0400

    cleanup

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Favorite < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :artwork
+  belongs_to :user
+  belongs_to :artwork
 
-    def self.ransackable_attributes(auth_object = nil)
-      ["artwork_id", "created_at", "id", "id_value", "updated_at", "user_id"]
-    end
+  def self.ransackable_attributes(auth_object = nil)
+    %w[artwork_id created_at id id_value updated_at user_id]
+  end
 end
